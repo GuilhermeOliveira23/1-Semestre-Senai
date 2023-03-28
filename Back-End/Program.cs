8dd93cd4214c028df8fa9faacb8c59a90185c369
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-03-28 17:21:42 -0300

    C#Demais

---
+++ Back-End/Program.cs
@@ -0,0 +1,130 @@
+﻿//variaveis
+
+
+
+
+
+
+//declarando uma variavel
+//tipo nome = valor
+// string nome ="Carlos";
+// const int idade = 36;
+
+// Console.WriteLine("A idade do " + nome + " e " + idade + " anos");
+
+//tipos de dados
+
+// int quantidade = 10;
+// double preco = 4.99D;
+// float altura = 1.80f;
+// bool careca = true;
+// Console.WriteLine(careca);
+// string texto = "Ola, mundo";
+// char letra = 'C';
+
+
+
+// //operador de atribuicao
+// int idade = 36;
+// Console.WriteLine(4 +"4");
+
+// //operadores aritmeticos
+
+// //multiplicacao
+// Console.WriteLine(5 * 3);
+
+// //divisao
+
+// Console.WriteLine(10 / 2);
+
+// //modular (Ve se e impar ou par)
+// Console.WriteLine(5 % 2);
+
+
+
+// //operadores de comparacao
+
+// //igual a
+// Console.WriteLine(5 == 6 );
+
+// //maior ou igual a 
+// Console.WriteLine(5 >= 4);
+
+// //menor ou igual a
+// Console.WriteLine(4 <= 3);
+
+// //maior que 
+// Console.WriteLine(5 > 1);
+
+// //menor que
+// Console.WriteLine(5 < 2);
+
+// //diferente de
+// Console.WriteLine(5 != 8);
+// //operadores logicos e tabela verdade
+// // && : e
+// //|| : ou
+// ! : negacao
+
+                    //tabela verdade
+
+        // Console.WriteLine(5 == 5 && 8 == 8);//TRUE && TRUE = TRUE
+        // Console.WriteLine(5 == 5 && 8 == 8);//TRUE && FALSE = FALSE
+        // Console.WriteLine(5 == 5 && 8 == 8);//FALSE && FALSE = FALSE
+
+        // Console.WriteLine(2 == 2 || 3 == 3);//TRUE || FALSE = TRUE
+        // Console.WriteLine(2 == 2 || 3 == 3); //TRUE || FALSE = TRUE
+        // Console.WriteLine(2 == 2 || 3 == 3); //FALSE || TRUE = TRUE
+        // Console.WriteLine(2 == 2 || 3 == 3); //FALSE || FALSE = FALSE
+
+
+
+///se voce nao e maior de idade
+        //  Console.WriteLine(!(2 == 2 || 3 == 3));
+
+
+//Crie um programa para calcular o IMC(indice de massa corporal) de uma Pessoa
+
+
+//Entradas
+string nome = "Carlos";
+float peso = 89.9f;
+float altura = 1.80f;
+
+//processamentos
+float imc = peso / (altura * altura);
+Console.WriteLine("O IMC do " + nome + " e de :" + Math.Round(imc,2));
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//soma
+
+
