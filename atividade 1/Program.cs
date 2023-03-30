398cb8f56cc0c7f54f6b59e62cf6c66051b1603e
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-03-30 17:27:34 -0300

    Estruturas condicionais

---
+++ atividade 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+int meses = idade * 12;
+int dias = meses * 30;
+int horas = dias * 24;
+int minutos = horas * 60;
+
+Console.WriteLine($"Voce tem {idade} anos, {meses} meses, {dias} dias, {horas} horas, {minutos} minutos.");
+
---
+++ atividade 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"Digite o ano de nascimento da pessoa: ");
+int anoNascimento = int.Parse(Console.ReadLine()!);
+
+//modulo para receber a data atual
+int anoAtual = DateTime.Now.Year;
+
+Console.WriteLine(anoAtual);
+
+//Calcular a idade da pessoa
+int idade = anoAtual - anoNascimento;
+int idadeSemana = idade * 52; // 52 numero de semanas 
+
+Console.WriteLine(@$"Idade: {idade} anos
+Idade em semanas: {idadeSemana} semanas");
