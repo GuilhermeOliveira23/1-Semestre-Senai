9d8ce3ce144d4e3546604484df76ab8af5db5226
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-04-04 17:15:57 -0300

    Estrutura de Repetição(04/04/23)

--- Estruturas-Repeticao/obj/Debug/net7.0/Nova pasta.AssemblyInfo.cs
+++ Estruturas-Repeticao/obj/Debug/net7.0/Nova pasta.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Programa ex")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Nova pasta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Programa ex")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Programa ex")]
+[assembly: System.Reflection.AssemblyProductAttribute("Nova pasta")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Nova pasta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
