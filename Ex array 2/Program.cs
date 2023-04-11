2f777d3bd24e3f6edad011a405f82e0bd2ad536c
Guilherme Gozzi Oliveira <olivergozzi@gmail.com>
2023-04-11 20:30:29 -0300

    Ex2Array

--- Ex array 2/Program.cs
+++ Ex array 2/Program.cs
@@ -18,33 +18,23 @@
 //4) nome: Julia
 //idade : 18 anos
 
+// Escreva um programa que receba e imprima o nome e idade de 5 pessoas (mude a cor do resultado)
 
+string[] nomes = new string[5];
+int[] idades = new int[5];
 
-string []nomePessoas = new string [5];
-int []idadePessoas = new int [5];
+for (int i = 0; i < 5; i++) {
+    Console.Write($"Digite o nome da {i + 1}º pessoa: ");
+    nomes[i] = Console.ReadLine()!;
 
-Console.ForegroundColor = ConsoleColor.Blue;
-
-
-
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"Digite o nome do {i+1}º participante");
-   nomePessoas[i] = Console.ReadLine()!;
-   
-   Console.WriteLine($"Digite a idade do {i+1}º participante");
-   idadePessoas[i] = int.Parse(Console.ReadLine()!);
-   
-   
-   
-    
+    Console.Write($"Digite a idade da {i + 1}º pessoa: ");
+    idades[i] = int.Parse(Console.ReadLine()!);
 }
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"{i+1} Nome");
-    
-    
+for (int i = 0; i < 5; i++) {
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine($"{i + 1}) Nome: {nomes[i]}");
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"Idade: {idades[i]} anos");
 }
-
 
