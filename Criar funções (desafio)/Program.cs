616e1e24dd7bb72bee13d5d0acf2e69470b1503d
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-04-18 17:20:50 -0300

    Projeto turismo finalizado e desafio do produto iniciado

---
+++ Criar funções (desafio)/Program.cs
@@ -0,0 +1,110 @@
+﻿// string nome = "";
+// float preco = 0;
+// bool promocao = false;
+
+
+
+// static void Menu(){
+
+    
+// }
+
+//Criar funcao MostrarMenu
+//Criar funcao CadastrarProduto
+//Criar funcao ListarProduto
+
+
+
+string[] Nomes = new string[10];
+float[] Precos = new float[10];
+string[] Marca = new string[10];
+int[] Quantidade = new int[10];
+
+static void Menu(){
+
+    Console.WriteLine(@$"Escolha o que deseja fazer : [Digite : 1/2/3]
+----------------------------------  
+|                                |
+|       (1)Cadastrar Produto     |
+|       (2)Listar Produto        |
+|       (3)Sair                  |              
+|                                |        
+|                                |                                                                                                      
+----------------------------------
+");
+
+string escolha = Console.ReadLine()!;
+
+ }
+
+
+
+static void CadastrarProduto(string escolha, string[]Nomes,float[] Precos , float[] Quantidade){
+
+
+switch (escolha)
+{
+    case "1":
+    for (var i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"Nome do produto:");
+        Nomes[i]= Console.ReadLine()!;
+        
+        Console.WriteLine($"Preco do produto:");
+        Precos[i] = float.Parse(Console.ReadLine()!);    
+        
+        Console.WriteLine($"Quantia do produto:");
+        Quantidade[i] = int.Parse(Console.ReadLine()!);     
+        
+        
+    }
+        break;
+        case "2":
+        for (int i = 0; i < 15; i++)
+        {   
+            Console.WriteLine($"{1 + i}º Produto:");
+            Console.WriteLine($" Nome: {Nomes[i]}");
+            Console.WriteLine($"Preco: {Precos[i]}");
+            Console.WriteLine($"Quantidade: {Quantidade[i]}");
+            
+        }
+        
+        break;
+        case "3": 
+        break;
+    default:
+        break;
+}
+
+
+
+}
+
+// if ternário
+
+
+
+    
+
+// string bola = "";
+// string mamute = "a";
+
+
+
+// static bool ValorMasculino(string a , string b){
+
+
+// if (a == b)
+// {
+//     return true;
+// }
+// else{
+//     Console.WriteLine($"Incorreto!!!");
+    
+//     return false;
+// }
+
+// };
+
+// bool senhaValida =  ValorMasculino(mamute, bola);
+
---
+++ Projeto Turismo/Program.cs
@@ -0,0 +1,135 @@
+﻿//Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+
+//Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
+//O sistema deve exibir um menu com as seguintes opções:
+
+//1- Cadastrar passagem
+//2- Listar Passagens
+//0- Sair
+//Observação :  Criar ao menos uma função (Efetuar Login).
+
+//Ao cadastrar uma passagem ao final o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N). 
+
+const string senhaCorreta = "123456";
+char continuarPrograma =' ';
+
+string[] nomes = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] data = new string[5];
+
+static bool EfetuarLogin(string senhaDigitada, string senhaCorreta)
+{     
+    if (senhaDigitada == senhaCorreta)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
+
+ bool resultado = false;
+
+do{
+
+Console.WriteLine($"Digite sua senha");
+string senha = Console.ReadLine()!;
+
+resultado = EfetuarLogin(senha,senhaCorreta);
+
+
+if (resultado != true)
+{
+    Console.WriteLine($"Senha invalida, tente novamente");
+    
+}
+
+
+
+}while(resultado == false);
+
+
+
+
+
+ menu: Console.WriteLine(@$"Escolha o que deseja fazer:
+---------------------------------------------
+|                                           |           
+|          (1) Cadastrar Passagens          |    
+|          (2) Listar Passagens             |
+|          (0) Sair                         |
+|                                           |
+|                                           |
+|                                           |
+---------------------------------------------
+
+
+");
+int numero = int.Parse(Console.ReadLine()!); 
+// era melhor ter usado string aqui, pois a escolha "int" faz com que a placa rode dois calculos ao inves de um
+
+
+
+
+
+switch (numero)
+{
+    case 1:
+
+    info:
+    for (var i = 0; i < 5; i++)
+    {
+        
+        Console.WriteLine($"Digite seu nome:");
+        nomes[i] = Console.ReadLine()!;
+
+        Console.WriteLine($"Informe sua origem:");
+        origem[i] = Console.ReadLine()!;
+
+        Console.WriteLine($"Informe seu destino:");
+        destino[i] = Console.ReadLine()!;
+
+        Console.WriteLine($"Informe a data [dia/mes/ano]:");
+        data[i] = Console.ReadLine()!;
+
+        
+
+        
+        
+              
+        
+    }
+        Console.WriteLine($"Deseja cadastrar uma nova passagem? [s/n]");
+        continuarPrograma = char.Parse(Console.ReadLine()!.ToUpper());
+     if (continuarPrograma == 'S' ){
+            goto info;
+        }
+        else {goto menu;};
+        
+        
+
+
+    case 2:
+    for (var i = 0; i < 5; i++)
+    {
+        Console.WriteLine(@$"{i + 1}º Nome: {nomes[i]}");     
+        Console.WriteLine($"{i + 1}º Origem: {origem[i]}");   
+        Console.WriteLine($"{i + 1}º Destino: {destino[i]}");   
+        Console.WriteLine($"{i + 1}º Data: {data[i]}"); 
+        Console.WriteLine($"");
+        
+
+    }
+
+    break;
+    case 0 : 
+    Environment.Exit(0);
+
+    break;
+        
+    default:
+        break;
+}
+
